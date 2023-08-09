3d48cb5008b702eb033756f9df9116f6cfab7508
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-09 18:03:11 +0300

    Commit with 01.Substraction

--- Dom-Events-Exercise/01. Subtraction/subtract.js
+++ Dom-Events-Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-  console.log("TODO:...");
+  const firstNum = Number(document.querySelector("#firstNumber").value);
+  const secondNum = Number(document.querySelector("#secondNumber").value);
+
+  const sum = firstNum - secondNum;
+
+  const result = document.querySelector("#result");
+  result.textContent = sum;
 }
