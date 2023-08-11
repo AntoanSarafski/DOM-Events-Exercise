fdb8edf699632626b96dec1c1e3081206b5cd729
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-11 12:07:25 +0300

    Commit 06.TableMatches with .classList and .includes

--- Dom-Events-Exercise/06. Table - Search Engine/solution.js
+++ Dom-Events-Exercise/06. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const searchQuery = document.querySelector("#searchField").value;
 
-   }
+    const cells = Array.from(document.querySelectorAll("tbody td"));
+
+    cells.forEach((c) => {
+      c.parentElement.classList.remove("select");
+    });
+
+    cells.forEach((c) => {
+      if (c.textContent.includes(searchQuery)) {
+        c.parentElement.classList.add("select");
+      }
+    });
+  }
 }
