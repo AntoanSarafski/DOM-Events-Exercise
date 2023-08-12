2ad4348e41e25b747b3087568f647a6fb725eea7
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-12 13:00:46 +0300

    Commit 08 with JSONparse, appendchild, checkbox.

--- Dom-Events-Exercise/08. Furniture/solution.js
+++ Dom-Events-Exercise/08. Furniture/solution.js
@@ -1,4 +1,43 @@
 function solve() {
+  const button = document.querySelector("#exercise button");
+  button.addEventListener("click", parseFurnitureInput);
+}
 
-  //TODO...
+function parseFurnitureInput() {
+  const input = JSON.parse(document.querySelector("#exercise textarea").value);
+  console.log(input);
+  const tableBody = document.querySelector("tbody");
+
+  input
+    .map((furniture) => {
+      const row = document.createElement("tr");
+
+      const imageCell = document.createElement("td");
+      const img = document.createElement("img");
+      img.src = furniture.img;
+      imageCell.appendChild(img);
+
+      const nameCell = document.createElement("td");
+      nameCell.textContent = furniture.name;
+
+      const priceCell = document.createElement("td");
+      priceCell.textContent = furniture.price;
+
+      const decorationFactorCell = document.createElement("td");
+      decorationFactorCell.textContent = furniture.decFactor;
+
+      const checkCell = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkCell.appendChild(checkbox);
+
+      row.appendChild(imageCell);
+      row.appendChild(nameCell);
+      row.appendChild(priceCell);
+      row.appendChild(decorationFactorCell);
+      row.appendChild(checkCell);
+
+      return row;
+    })
+    .forEach((row) => tableBody.appendChild(row));
 }
