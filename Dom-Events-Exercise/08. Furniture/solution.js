0ead2f3e41f9884c47741a0f47944c05f5bb4002
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-12 13:20:14 +0300

    Commit 08 with all functions in one object.

--- Dom-Events-Exercise/08. Furniture/solution.js
+++ Dom-Events-Exercise/08. Furniture/solution.js
@@ -18,36 +18,36 @@ function parseFurnitureInput() {
       const decorationFactorCell = document.createElement("td");
       decorationFactorCell.textContent = furniture.decFactor;
 
-      row.appendChild(createImageCell(furniture.img));
-      row.appendChild(createTextCell(furniture.name));
-      row.appendChild(createTextCell(furniture.price));
-      row.appendChild(createTextCell(furniture.decFactor));
-      row.appendChild(createCheckboxCell());
+      row.appendChild(cellCreator.createImageCell(furniture.img));
+      row.appendChild(cellCreator.createTextCell(furniture.name));
+      row.appendChild(cellCreator.createTextCell(furniture.price));
+      row.appendChild(cellCreator.createTextCell(furniture.decFactor));
+      row.appendChild(cellCreator.createCheckboxCell());
 
       return row;
     })
     .forEach((row) => tableBody.appendChild(row));
 }
 
-function createImageCell(src) {
-  const imageCell = document.createElement("td");
-  const img = document.createElement("img");
-  img.src = src;
-  imageCell.appendChild(img);
-  return imageCell;
-}
+const cellCreator = {
+  createImageCell(src) {
+    const imageCell = document.createElement("td");
+    const img = document.createElement("img");
+    img.src = src;
+    imageCell.appendChild(img);
+    return imageCell;
+  },
+  createTextCell(text) {
+    const cell = document.createElement("td");
+    cell.textContent = text;
+    return cell;
+  },
+  createCheckboxCell() {
+    const checkCell = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkCell.appendChild(checkbox);
 
-function createTextCell(text) {
-  const cell = document.createElement("td");
-  cell.textContent = text;
-  return cell;
-}
-
-function createCheckboxCell() {
-  const checkCell = document.createElement("td");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkCell.appendChild(checkbox);
-
-  return checkCell;
-}
+    return checkCell;
+  },
+};
