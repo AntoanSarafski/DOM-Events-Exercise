6b7fb29c7b482f5176851be508812a821ecfbc8f
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-12 13:28:27 +0300

    Update with function createFurnitureRow.

--- Dom-Events-Exercise/08. Furniture/solution.js
+++ Dom-Events-Exercise/08. Furniture/solution.js
@@ -8,25 +8,19 @@ function parseFurnitureInput() {
   console.log(input);
   const tableBody = document.querySelector("tbody");
 
-  input
-    .map((furniture) => {
-      const row = document.createElement("tr");
+  input.map(createFurnitureRow).forEach((row) => tableBody.appendChild(row));
+}
 
-      const priceCell = document.createElement("td");
-      priceCell.textContent = furniture.price;
+function createFurnitureRow(furniture) {
+  const row = document.createElement("tr");
 
-      const decorationFactorCell = document.createElement("td");
-      decorationFactorCell.textContent = furniture.decFactor;
+  row.appendChild(cellCreator.createImageCell(furniture.img));
+  row.appendChild(cellCreator.createTextCell(furniture.name));
+  row.appendChild(cellCreator.createTextCell(furniture.price));
+  row.appendChild(cellCreator.createTextCell(furniture.decFactor));
+  row.appendChild(cellCreator.createCheckboxCell());
 
-      row.appendChild(cellCreator.createImageCell(furniture.img));
-      row.appendChild(cellCreator.createTextCell(furniture.name));
-      row.appendChild(cellCreator.createTextCell(furniture.price));
-      row.appendChild(cellCreator.createTextCell(furniture.decFactor));
-      row.appendChild(cellCreator.createCheckboxCell());
-
-      return row;
-    })
-    .forEach((row) => tableBody.appendChild(row));
+  return row;
 }
 
 const cellCreator = {
